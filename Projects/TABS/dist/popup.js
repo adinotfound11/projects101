05cbf083e869898db079b2e92de7911d1723481b
adinotfound11 <aadtiyapatil@gmail.com>
2025-06-27 20:39:53 +0530

    Removed TABS

--- Projects/TABS/dist/popup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-document.getElementById("groupNow")?.addEventListener("click", async () => {
-    try {
-        await chrome.runtime.sendMessage({ action: "groupTabs" });
-    }
-    catch (error) {
-        console.error("Error sending message to background:", error);
-    }
-});
--- Projects/TABS/popup.ts
+++
@@ -1,7 +0,0 @@
-document.getElementById("groupNow")?.addEventListener("click", async () => {
-  try {
-    await chrome.runtime.sendMessage({ action: "groupTabs" });
-  } catch (error) {
-    console.error("Error sending message to background:", error);
-  }
-});
